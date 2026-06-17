244f3fd7b6ccdffe34c28466838412eb6ee700f6
jzzj-KKBK <1020050112@qq.com>
2026-06-17 13:45:24 +0800

    1.3 同步上游

--- tests/TestChar.py
+++ tests/TestChar.py
@@ -268,6 +268,28 @@ class TestChar(TaskTestCase):
         self.assertTrue(current.has_intro)
         self.assertFalse(current.has_sub_dps_intro)
 
+    def test_switch_target_skips_dead_char_and_allows_recovered_char(self):
+        class Task:
+            def time_elapsed_accounting_for_freeze(self, start, intro_motion_freeze=False):
+                if start < 0:
+                    return 10000
+                return time.time() - start
+
+        task = Task()
+        combat = AutoCombatTask.__new__(AutoCombatTask)
+        current = BaseChar(task, 0, char_type=CharType.MAIN_DPS)
+        healer = BaseChar(task, 1, char_type=CharType.HEALER)
+        sub_dps = BaseChar(task, 2, char_type=CharType.SUB_DPS)
+        combat.chars = [current, healer, sub_dps]
+        combat.char_alive_state = [True, False, True]
+        combat.char_dead_votes = [0, 0, 0]
+        combat.char_alive_votes = [0, 0, 0]
+
+        self.assertEqual(combat._choose_switch_target(current, False), sub_dps)
+
+        combat.set_char_alive(healer, True)
+        self.assertEqual(combat._choose_switch_target(current, False), healer)
+
     def test_chisa_support_intro_records_buff_and_switches_immediately(self):
         class Task:
             char_config = {'Chisa DPS': False}
@@ -630,6 +652,35 @@ class TestChar(TaskTestCase):
         aemeath.perform_everything()
         self.assertEqual(aemeath.actions, ['lib1'])
         self.assertEqual(aemeath.intro_liberation_time, -1)
+
+    def test_aemeath_enhance_e_only_sends_one_resonance_key(self):
+        class Task:
+            def __init__(self):
+                self.keys = []
+                self.skip_combat_check = False
+
+            def time_elapsed_accounting_for_freeze(self, start, intro_motion_freeze=False):
+                return time.time() - start
+
+            def find_one(self, template, threshold=None):
+                return template == 'aemeath_e1'
+
+            def get_resonance_key(self):
+                return 'e'
+
+            def send_key(self, key, interval=-1, down_time=0.01, after_sleep=0):
+                self.keys.append(key)
+
+            def sleep(self, sec):
+                pass
+
+        aemeath = Aemeath(Task(), 0)
+        self.assertTrue(aemeath.click_enhance_e_once())
+        aemeath.record_enhance_e()
+        self.assertEqual(aemeath.task.keys, ['e'])
+
+        self.assertFalse(aemeath.click_enhance_e_once())
+        self.assertEqual(aemeath.task.keys, ['e'])
 
     def test_switch_priority_hooks(self):
         class Task:
@@ -1013,6 +1064,9 @@ class TestChar(TaskTestCase):
             def is_con_full(self):
                 return False
 
+            def get_current_con(self):
+                return 0
+
             def click_resonance(self, **kwargs):
                 self.resonance_clicks += 1
                 return True, 0, False
@@ -1096,6 +1150,65 @@ class TestChar(TaskTestCase):
 
         linnai = Linnai(Task(), 0)
         self.assertTrue(linnai.check_res())
+
+    def test_linnai_switches_without_attack_when_con_ready(self):
+        class Task:
+            pass
+
+        class TestLinnai(Linnai):
+            def __init__(self):
+                super().__init__(Task(), 0)
+                self.has_intro = True
+                self.clicks = 0
+                self.switches = 0
+
+            def get_current_con(self):
+                return 1
+
+            def click(self, *args, **kwargs):
+                self.clicks += 1
+
+            def liberation_available(self):
+                return False
+
+            def switch_next_char(self, *args, **kwargs):
+                self.switches += 1
+
+        linnai = TestLinnai()
+        linnai.do_perform()
+        self.assertEqual(linnai.clicks, 0)
+        self.assertEqual(linnai.switches, 1)
+
+    def test_linnai_intro_attack_stops_when_con_full(self):
+        class Task:
+            pass
+
+        class TestLinnai(Linnai):
+            def __init__(self):
+                super().__init__(Task(), 0)
+                self.has_intro = True
+                self.normal_attack_until_con_full = False
+                self.switches = 0
+
+            def get_current_con(self):
+                return 0
+
+            def check_res(self):
+                return True
+
+            def continues_normal_attack(self, duration, **kwargs):
+                self.normal_attack_until_con_full = kwargs.get('until_con_full', False)
+
+            def liberation_available(self):
+                return False
+
+            def switch_next_char(self, *args, **kwargs):
+                self.switches += 1
+
+        linnai = TestLinnai()
+        linnai.do_perform()
+        self.assertTrue(linnai.normal_attack_until_con_full)
+        self.assertEqual(linnai.switches, 1)
 
     def test_intro_does_not_switch_to_phrolova_during_liberation_lock(self):
         class Task:
